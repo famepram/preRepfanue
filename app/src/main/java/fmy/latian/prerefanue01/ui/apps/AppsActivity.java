5e346e2a194d830633f223f0a61049c95d55b076
femmy_san <femmy.pramana@gmail.com>
2017-08-11 17:17:36 +0700

    latest

--- app/src/main/java/fmy/latian/prerefanue01/ui/apps/AppsActivity.java
+++ app/src/main/java/fmy/latian/prerefanue01/ui/apps/AppsActivity.java
@@ -2,14 +2,41 @@ package fmy.latian.prerefanue01.ui.apps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
 
 import fmy.latian.prerefanue01.R;
+import fmy.latian.prerefanue01.data.models.Apps;
 
-public class AppsActivity extends AppCompatActivity {
+public class AppsActivity extends AppCompatActivity implements AppsContract.view{
+
+    private ListView lvListApp;
+    private Button btnAddApps;
+
+    private AppsListAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_apps);
+        initUI();
+    }
+
+    private void initUI(){
+        lvListApp = (ListView) findViewById(R.id.listview_apps);
+        btnAddApps = (Button) findViewById(R.id.btn_add_apps);
+    }
+
+    @Override
+    public void onListLoaded(List<Apps> e) {
+
+    }
+
+    @Override
+    public void onListEmpty() {
+
     }
 }
--- app/src/main/java/fmy/latian/prerefanue01/ui/apps/AppsListAdapter.java
+++ app/src/main/java/fmy/latian/prerefanue01/ui/apps/AppsListAdapter.java
@@ -1,8 +1,36 @@
 package fmy.latian.prerefanue01.ui.apps;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
 /**
  * Created by Femmy on 8/11/2017.
  */
 
-public class AppsListAdapter {
+public class AppsListAdapter extends BaseAdapter{
+
+    public AppsListAdapter(){
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
